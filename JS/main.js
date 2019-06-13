22d5683f4fc306d447ad10f8569d5001381ef24d
John Hayden <jthayden10@me.com>
2019-06-13 15:33:46 -0400

    added JS object and array for questions

--- JS/main.js
+++ JS/main.js
@@ -1,45 +1,75 @@
 let gameBoard = document.querySelector('.game-board');
 
-for( let i = 0; i<5; i++){
-    let categories = document.createElement('category');
+for (let i = 0; i < 5; i++) {
+    let categories = document.createElement('h2');
     categories.classList.add('category');
     gameBoard.appendChild(categories);
     categories.appendChild(document.createTextNode('Category'));
 }
-for(let i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     let btn = document.createElement('button');
     btn.classList.add('square');
+    btn.dataset.column = i
+    btn.dataset.row = 'one'
     gameBoard.appendChild(btn);
     btn.appendChild(document.createTextNode('$100'));
 }
-for(let i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     let btn = document.createElement('button');
     btn.classList.add('square');
+    btn.dataset.column = i
+    btn.dataset.row = 'two'
     gameBoard.appendChild(btn);
     btn.appendChild(document.createTextNode('$200'));
 }
-for(let i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     let btn = document.createElement('button');
     btn.classList.add('square');
+    btn.dataset.column = i
+    btn.dataset.row = 'three'
     gameBoard.appendChild(btn);
     btn.appendChild(document.createTextNode('$300'));
 }
-for(let i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     let btn = document.createElement('button');
     btn.classList.add('square');
+    btn.dataset.column = i
+    btn.dataset.row = 'four'
     gameBoard.appendChild(btn);
     btn.appendChild(document.createTextNode('$400'));
 }
-for(let i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     let btn = document.createElement('button');
     btn.classList.add('square');
+    btn.dataset.column = i
+    btn.dataset.row = 'five'
     gameBoard.appendChild(btn);
     btn.appendChild(document.createTextNode('$500'));
 }
 
 
-$(document).ready(function(){
-$('.square').on('click', function(evt){
-   console.log(evt.target)
+$(document).ready(function () {
+    $('.square').on('click', function (evt) {
+        // console.log(evt.target)
+        console.log(questions[this.dataset.row][this.dataset.column])
+    })
+
+
 })
-})
+
+const questions = {
+    one: [
+        {
+            question: 'Does this work',
+            options: [
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        }
+    ]
+}
