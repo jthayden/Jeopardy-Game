927706290ae7ba2ab986edef1d56a697bfef3058
John Hayden <jthayden10@me.com>
2019-06-13 12:20:06 -0400

    fixed loop to create game board, added text in game tiles

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,33 @@
 let gameBoard = document.querySelector('.game-board');
 
-for(let i = 0; i < 25; i++) {
+for(let i = 0; i < 5; i++) {
     let btn = document.createElement('button');
     btn.classList.add('square');
     gameBoard.appendChild(btn);
+    btn.appendChild(document.createTextNode('$100'));
 }
+for(let i = 0; i < 5; i++) {
+    let btn = document.createElement('button');
+    btn.classList.add('square');
+    gameBoard.appendChild(btn);
+    btn.appendChild(document.createTextNode('$200'));
+}
+for(let i = 0; i < 5; i++) {
+    let btn = document.createElement('button');
+    btn.classList.add('square');
+    gameBoard.appendChild(btn);
+    btn.appendChild(document.createTextNode('$300'));
+}
+for(let i = 0; i < 5; i++) {
+    let btn = document.createElement('button');
+    btn.classList.add('square');
+    gameBoard.appendChild(btn);
+    btn.appendChild(document.createTextNode('$400'));
+}
+for(let i = 0; i < 5; i++) {
+    let btn = document.createElement('button');
+    btn.classList.add('square');
+    gameBoard.appendChild(btn);
+    btn.appendChild(document.createTextNode('$500'));
+}
+
