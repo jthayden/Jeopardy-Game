a754a2ae5d94ad1a65c5dd522cc639f9879084c9
John Hayden <jthayden10@me.com>
2019-06-14 10:09:02 -0400

    added array and for loop for categories

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,13 @@
 let gameBoard = document.querySelector('.game-board');
-
+const categoriesArray = ['cat1', 'cat2', 'cat3', 'cat4', 'cat5']
 for (let i = 0; i < 5; i++) {
     let categories = document.createElement('h2');
     categories.classList.add('category');
     gameBoard.appendChild(categories);
-    categories.appendChild(document.createTextNode('Category'));
+    categories.appendChild(document.createTextNode(categoriesArray[0]));
+}
+for(let i =0; i < 5; i++){
+    console.log(categoriesArray[i])
 }
 for (let i = 0; i < 5; i++) {
     let btn = document.createElement('button');
@@ -57,8 +60,66 @@ $(document).ready(function () {
 
 })
 
+
+
 const questions = {
     one: [
+        {
+            question: 'Does this work',
+            options: [
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        }
+    ],
+    two: [
+        {
+            question: 'Does this work',
+            options: [
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        }
+    ],
+    three: [
+        {
+            question: 'Does this work',
+            options: [
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        }
+    ],
+    four: [
+        {
+            question: 'Does this work',
+            options: [
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        }
+    ],
+    five: [
         {
             question: 'Does this work',
             options: [
